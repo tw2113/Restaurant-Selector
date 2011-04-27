b7083b4b458170a04d7ae994cfde62466f47de3c
Michael Beckwith <tw2113@gmail.com>
2011-04-26 21:08:18 -0500

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+This is simply a script used for when you are having a hard time figuring out where you want to go to eat. Configure it by adding your favorite restaurants to the array, and then when you load it in a browser, it will randomly show one of the entries for you. Then you go eat at which ever spot was shown. Easy :).
+
+Change however you want, doesn't bother me.
---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en-us" class="no-js">
+<head>
+<meta charset="utf-8" />
+	<title>Random Restaurant Selector</title>
+<style>
+<!--Feel free to style as you please-->
+body {background: #FFFEF2; color: #2359FF;}
+article {background: #AFBAFF; display:block; border-radius: 15px; margin: 0 auto; padding: 15px; width: 75%; text-align: center;}
+h2 {margin-top: 15px; height: 25px;}
+</style>
+<script src="js/modernizr.custom.js"></script><!--Includes for older browsers to understand the Article tag, and add feature detection if you want to get fancy-->
+</head>
+<body>
+    <?php
+    $restaurants = array(
+        //Enter restaurants that are near you that you enjoy going to
+        'Fast food chain',
+        'Sit down restaurant',
+        'convenience store',
+        'get creative with places',
+        'Drive home for lunch and eat there',
+        'Get back to work, you do not have time for lunch!',
+        '' //Left blank for "wildcard" instance
+        );
+    $randrestaurant = array_rand(array_flip($restaurants),1);
+    ?>
+	<article>
+	<h1>Where should I go to eat today?</h1>
+	<h2><?php echo $randrestaurant; ?></h2>
+	<p>A blank spot means it's a wildcard and someone else decides for you.
+	<br/>Refresh to see another option.</p>
+	
+    <p>Source at: <a href="#" title="Restaurant Selector Github Repo">Github</a>
+	</article>
+</body>
+</html>
